f1c1980767317602379cb6a852aafb4f6bbcfaf4
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-07-11 17:15:29 +0200

    Completed basic request

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,16 @@
         <main>
             <div class="container">
                 <div class="current_series">CURRENT SERIES</div>
+                <ul>
+                    @foreach ($fumetti as $fumetto)
+                        <li>
+                            <div>
+                                <img src="{{ $fumetto['thumb'] }}" alt="thumb del fumetto">
+                            </div>
+                            <h4>{{ $fumetto['title'] }}</h4>
+                        </li>
+                    @endforeach
+                </ul>
                 <div class="button_loadmore">LOAD MORE</div>
             </div>
         </main>
